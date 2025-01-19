d8269101fb7cce8b4f92909f9dab26b5d187c55c
Ludosiellll <denislabyncev935@gmail.com>
2025-01-19 20:08:26 +0300

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -2,41 +2,54 @@
 #include <Windows.h>
 #include <ctime> 
 #include <cstdlib> 
+#include <algorithm>
 
 using namespace std;
 
-
 int main()
 {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
-	srand(static_cast<unsigned int>(time(0)));
-	std::array<Card, 52> deck;
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    std::array<Card, 52> deck;
 
-	int card = 0;
-	for (int suit = 0; suit < MAX_SUITS; ++suit)
-		for (int rank = 0; rank < MAX_RANKS; ++rank)
-		{
-			deck[card].suit = static_cast<CardSuit>(suit);
-			deck[card].rank = static_cast<CardRank>(rank);
-			++card;
-		}
-	shuffleDeck(deck);
+    int card = 0;
+    for (int suit = 0; suit < MAX_SUITS; ++suit)
+        for (int rank = 0; rank < MAX_RANKS; ++rank)
+        {
+            deck[card].suit = static_cast<CardSuit>(suit);
+            deck[card].rank = static_cast<CardRank>(rank);
+            ++card;
+        }
+    while (true) {
+        shuffleDeck(deck);
 
-	int result = playBlackjack(deck);
+        int result = playBlackjack(deck);
 
-	if (result == WIN)
-	{
-		std::cout << "Ты выиграл!\n";
-	}
-	else if (result == NET)
-	{
-		std::cout << "Ничья!\n";
-	}
-	else
-	{
-		std::cout << "Ты проиграл...\n";
-	}
+        if (result == WIN) {
+            std::cout << "Ты выиграл!\n";
+        }
+        else if (result == NET) {
+            std::cout << "Ничья!\n";
+        }
+        else {
+            std::cout << "Ты проиграл...\n";
+        }
 
-	return 0;
+        cout << endl;
+        cout << "Вы хотите попробовать ещё раз?" << endl;
+        cout << "Введите (да/нет): ";
+
+        char choice[10]; 
+        cin >> choice;
+
+        if (!Choicing(choice)) 
+        {
+            cout << endl;
+            cout << "Игра окончена. Удачи!";
+            cout << endl;
+            break;
+        }
+        cout << endl;
+    }
+    return 0;
 }
